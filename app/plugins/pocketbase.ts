d047d8de46e8ec317278d4844062ac3ccad8f4df
Linus Fischer <leicraft@leicraftmc.de>
2025-11-24 11:59:49 +0100

    initial commit

---
+++ app/plugins/pocketbase.ts
@@ -0,0 +1,37 @@
+// plugins/pocketbase.js
+import PocketBase from 'pocketbase';
+
+export default defineNuxtPlugin(async () => {
+    const pb = new PocketBase(useRuntimeConfig().public.pocketbaseUrl);
+
+    const cookie = useCookie('pb_auth', {
+        path: '/',
+        secure: true,
+        sameSite: 'strict',
+        httpOnly: false, // change to "true" if you want only server-side access
+        maxAge: 604800,
+    })
+
+    // load the store data from the cookie value
+    pb.authStore.save((cookie.value as any)?.token, (cookie.value as any).record);
+
+    // send back the default 'pb_auth' cookie to the client with the latest store state
+    pb.authStore.onChange(() => { //@ts-ignore
+        cookie.value = {
+            token: pb.authStore.token,
+            record: pb.authStore.record,
+        };
+    });
+
+    try {
+        // get an up-to-date auth store state by verifying and refreshing the loaded auth model (if any)
+        pb.authStore.isValid && await pb.collection('users').authRefresh();
+    } catch (_) {
+        // clear the auth store on failed refresh
+        pb.authStore.clear();
+    }
+
+    return {
+        provide: { pb }
+    }
+});
